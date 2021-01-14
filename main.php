7e81fdea0fd521c4f6135ddba028e8755d957c1d
Daniel <daniel.kaminski123@gmail.com>
2021-01-14 13:31:24 +0100

    Added widget

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: ZSPlugin
 Plugin URI: https://zs-belzyce.edu.pl/
 Description: Plugin do wyświetlania przedmiotów nauczanych przez nauczycieli oraz kadry.
-Version: 1.3
+Version: 1.4
 Author: <a href="https://github.com/KaminskiDaniell">Daniel Kamiński</a>, <a href="https://github.com/jakubZlotek">Jakub Złotek</a>
 Author URI: https://jajuwa.xyz/
 License: MIT
@@ -259,3 +259,81 @@ function subpagesRenderer() {
 }
 
 add_action( 'init', 'register_subpages_block' );
+
+/*
+┌──────────────────────────────────────────────────────────────────────────────────┐
+│                                                                                  │
+│                         Blok dodający customowy widget                           │
+│                                                                                  │
+└──────────────────────────────────────────────────────────────────────────────────┘
+*/
+
+class Free_Days_Widget extends WP_Widget {
+ 
+    function __construct() {
+ 
+        parent::__construct(
+            'free-days-widget',  // Base ID
+            'Dni Wolne Widget'   // Name
+        );
+ 
+        add_action( 'widgets_init', function() {
+            register_widget( 'Free_Days_Widget' );
+        });
+ 
+    }
+ 
+    public $args = array(
+        'before_title'  => '<h4 class="widgettitle">',
+        'after_title'   => '</h4>',
+        'before_widget' => '<div class="widget-wrap">',
+        'after_widget'  => '</div></div>'
+    );
+ 
+    public function widget( $args, $instance ) {
+ 
+        echo $args['before_widget'];
+ 
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+        }
+ 
+        echo '<div class="textwidget">';
+ 
+        echo esc_html__( $instance['text'], 'text_domain' );
+ 
+        echo '</div>';
+ 
+        echo $args['after_widget'];
+ 
+    }
+ 
+    public function form( $instance ) {
+ 
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( '', 'text_domain' );
+        $text = ! empty( $instance['text'] ) ? $instance['text'] : esc_html__( '', 'text_domain' );
+        ?>
+        <p>
+        <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php echo esc_html__( 'Title:', 'text_domain' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'Text' ) ); ?>"><?php echo esc_html__( 'Text:', 'text_domain' ); ?></label>
+            <textarea class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'text' ) ); ?>" type="text" cols="30" rows="10"><?php echo esc_attr( $text ); ?></textarea>
+        </p>
+        <?php
+ 
+    }
+ 
+    public function update( $new_instance, $old_instance ) {
+ 
+        $instance = array();
+ 
+        $instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['text'] = ( !empty( $new_instance['text'] ) ) ? $new_instance['text'] : '';
+ 
+        return $instance;
+    }
+ 
+}
+$my_widget = new Free_Days_Widget();
